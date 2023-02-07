1310499386b340b764c5d5cd7c9f84ba745106e3
Nihisil <alexey@nihisil.com>
2023-02-07 15:09:43 +0700

    [#1] Disable not relevant credo rule

--- .credo.exs
+++ .credo.exs
@@ -156,12 +156,6 @@
         #
         # Controversial and experimental checks (opt-in, just replace `false` with `[]`)
         #
-        {Credo.Check.Readability.StrictModuleLayout,
-         [
-           order:
-             ~w/shortdoc moduledoc behaviour use import alias require module_attribute defstruct callback public_fun private_fun/a,
-           ignore: ~w/callback_impl/a
-         ]},
         {Credo.Check.Consistency.MultiAliasImportRequireUse, []},
         {Credo.Check.Consistency.UnusedVariableNames, false},
         {Credo.Check.Design.DuplicatedCode, files: %{excluded: ["**/*_test.exs"]}},
