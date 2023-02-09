b61f4d0ada01e2e4849c4a018acb1eabb46c8b20
Aleksei Lisikhin <475367+Nihisil@users.noreply.github.com>
2023-02-09 18:23:09 +0700

    [#5] Cache number of likes (#20)

--- lib/sample_app_web/controllers/page_controller.ex
+++ lib/sample_app_web/controllers/page_controller.ex
@@ -4,14 +4,27 @@ defmodule SampleAppWeb.PageController do
 
   alias SampleApp.Social.Likes
 
-  def index(conn, _params) do
-    likes_count = Likes.get_number_of_likes()
+  @cache_likes_key "likes_count"
 
-    render(conn, "index.html", %{:likes_count => likes_count})
+  def index(conn, _params) do
+    {likes_count, from_cache} =
+      case SampleApp.RedisCache.get(@cache_likes_key) do
+        nil ->
+          likes_count = Likes.get_number_of_likes()
+          SampleApp.RedisCache.put(@cache_likes_key, likes_count)
+
+          {likes_count, false}
+
+        likes_count ->
+          {likes_count, true}
+      end
+
+    render(conn, "index.html", %{:likes_count => likes_count, :from_cache => from_cache})
   end
 
   def create(conn, _params) do
     Likes.create_like()
+    SampleApp.RedisCache.delete(@cache_likes_key)
 
     redirect(conn, to: Routes.page_path(conn, :index))
   end
